e667e27d7ebe82d51d732c4a05e1b17e555077f9
devMahmoud <eng.mahmoud.mohamed93@gmail.com>
2022-04-08 23:38:16 +0200

    make popup dynamic

--- popup.js
+++ popup.js
@@ -1,4 +1,15 @@
 const closePop = document.querySelector('.cross-popup-icon');
+const popupcontainer = document.createElement('section');
+const diviconimage = document.createElement('div');
+const divbackcross = document.createElement('div');
+const imgcross = document.createElement('img');
+const divpopupimage = document.createElement('div');
+const popupimage = document.createElement('img');
+const titleandtech = document.createElement('div');
+const divtitle = document.createElement('div');
+const popuptitle = document.createElement('h1');
+const description = document.createElement('div');
+
 
 function cards() {
   const gridContainer = document.querySelector('.grid');
@@ -110,55 +121,56 @@ const projects = [
   }
 ];
 
-const popupcontainer = document.createElement('section');
+const createPopup = (name, img, desc, tech) => {
+  const divul = document.createElement('ul');
+  console.log("iside creat pop")
 popupcontainer.classList.add('mobile-popup-container');
 popupcontainer.classList.add('display-none');
 
-const diviconimage = document.createElement('div');
+
 diviconimage.classList.add('icon-image');
 
-const divbackcross = document.createElement('div');
+
 divbackcross.classList.add('back-cross');
 
-const imgcross = document.createElement('img');
+
 imgcross.classList.add('cross-popup-icon');
 imgcross.src = './images/cross-icon.png';
 imgcross.alt = '';
 
-const divpopupimage = document.createElement('div');
+
 divpopupimage.classList.add('popup-image');
 
-const popupimage = document.createElement('img');
-popupimage.src = projects[0].imageUrl;
 
-const titleandtech = document.createElement('div');
+popupimage.src = img;
+
+
 titleandtech.classList.add('title-and-tech');
 
-const divtitle = document.createElement('div');
-const popuptitle = document.createElement('h1');
-popuptitle.innerText = projects[0].title;
+
+popuptitle.innerText = name;
 popuptitle.classList.add('popup-title');
 
+for (let i = 0; i < tech.length; i += 1) {
+  console.log('loop throw tech list')
+  const divli = document.createElement('li');
+  console.log(divli)
+  divli.innerText = tech[i];
+  divli.classList.add('popup-tech-li');
+  divul.appendChild(divli);
+  console.log(divul)
+}
+
+
+description.classList.add('popup-description');
+description.innerText = desc;
+
+
 const divtech = document.createElement('div');
-const divul = document.createElement('ul');
+
 divul.classList.add('popup-tech');
 
-const divli1 = document.createElement('li');
-divli1.innerText = 'Html';
-divli1.classList.add('popup-tech-li');
-const divli2 = document.createElement('li');
-divli2.innerText = 'Ruby in rails';
-divli2.classList.add('popup-tech-li');
-const divli3 = document.createElement('li');
-divli3.innerText = 'css';
-divli3.classList.add('popup-tech-li');
-const divli4 = document.createElement('li');
-divli4.innerText = 'Javascript';
-divli4.classList.add('popup-tech-li');
 
-const description = document.createElement('div');
-description.classList.add('popup-description');
-description.innerText = projects[0].description;
 
 const buttons = document.createElement('div');
 buttons.classList.add('popup-buttons');
@@ -192,10 +204,7 @@ titleandtech.appendChild(divtitle);
 divtitle.appendChild(popuptitle);
 titleandtech.appendChild(divtech);
 divtech.appendChild(divul);
-divul.appendChild(divli1);
-divul.appendChild(divli2);
-divul.appendChild(divli3);
-divul.appendChild(divli4);
+
 
 popupcontainer.appendChild(description);
 popupcontainer.appendChild(buttons);
@@ -205,11 +214,14 @@ live.appendChild(imglive);
 buttons.appendChild(source);
 source.appendChild(psource);
 source.appendChild(imgsource);
+}
 
 const storybutton = document.querySelectorAll('.seepr-button');
 
 for (let i = 0; i < storybutton.length; i += 1) {
   storybutton[i].addEventListener('click', () => {
+    console.log('show popup')
+    createPopup(projects[i].title, projects[i].imageUrl, projects[i].description, projects[i].technologies);
     popupcontainer.classList.remove('display-none');
     popupcontainer.classList.add('display-flex');
   });
