7991d9bb6269a93b45706a39e7fc124f1522c03f
ABDULALI3468 <mrabdulali2003@gmail.com>
2022-09-04 20:24:28 +0500

    Used modern CSS attributes for animation if Navbar.

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 const hamburger = document.querySelector(".hamburger");
+const header = document.querySelector(".header-desktop");
 const mobileMenu = document.querySelector(".header-desktop");
 const crossicon = document.querySelector(".cross-icon");
 const about = document.getElementById("about");
@@ -6,26 +7,36 @@ const contact = document.getElementById("contact");
 const portfolio = document.getElementById("portfolio");
 
 hamburger.addEventListener("click", () => {
-  mobileMenu.style.display = "flex";
+  // mobileMenu.style.display = "flex";
   document.body.style.overflow = "hidden";
+
+  const visibility = header.getAttribute("data-visible");
+  if (visibility === "false") {
+    header.setAttribute("data-visible", true);
+    console.log('header is false')
+  } else if (visibility === "true") {
+    header.setAttribute("data-visible", false);
+    console.log('header is true')
+  }
 });
 
 crossicon.addEventListener("click", () => {
-  mobileMenu.style.display = "none";
+  // mobileMenu.style.display = "none";
+  header.setAttribute("data-visible", false);
   document.body.style.overflow = "auto";
 });
 
 portfolio.addEventListener("click", () => {
-  mobileMenu.style.display = "none";
+  header.setAttribute("data-visible", false);
   document.body.style.overflow = "auto";
 });
 
 about.addEventListener("click", () => {
-  mobileMenu.style.display = "none";
+  header.setAttribute("data-visible", false);
   document.body.style.overflow = "auto";
 });
 
 contact.addEventListener("click", () => {
-  mobileMenu.style.display = "none";
+  header.setAttribute("data-visible", false);
   document.body.style.overflow = "auto";
 });
